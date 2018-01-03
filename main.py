02b2671097666086182b97bc3fec48887d093918
Tsemharb <gorbachevart@gmail.com>
2018-01-03 20:40:37 +0300

    minefields being generated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QDesktopWidget, QWidget
+from PyQt5.QtWidgets import QApplication, QDesktopWidget, QWidget, QMessageBox
 from start_window import Ui_Form
 from minefield import Minefield
 import time
@@ -10,19 +10,8 @@ class StartForm(Ui_Form):
         self.setupUi(form)
         self.center(form)
         # self.ok_btn.clicked.connect(self.create_minefield)
-        self.ok_btn.clicked.connect(lambda: self.crr(form))
+        self.ok_btn.clicked.connect(lambda: self.create_minefield(form))
         form.show()
-
-    def crr(self, form):
-        # form.hide()
-        # time.sleep(2)
-
-        self.minefield = Minefield()
-        #self.minefield.setParent(self)
-        self.minefield.show()
-
-        #time.sleep(3)
-        #form.show()
 
     def center(self, form):
         qr = form.frameGeometry()
@@ -30,22 +19,37 @@ class StartForm(Ui_Form):
         qr.moveCenter(cp)
         form.move(qr.topLeft())
 
-    def create_minefield(self):
+    def create_minefield(self, form):
         if self.easy_rbtn.isChecked():
-            mode = 1
+            mode = (8, 8, 16)
         elif self.medium_rbtn.isChecked():
-            mode = 2
+            mode = (16, 16, 40)
         elif self.hard_rbtn.isChecked():
-            mode = 3
+            mode = (16, 30, 99)
         elif self.custom_rbtn.isChecked():
             mode = 4
         else:
             mode = 5
-        self.mode_lbl.setText(str(mode))
+        #self.mode_lbl.setText(str(mode))
 
-        minefield = Minefield()
-        minefield.setParent(self)
-        minefield.show()
+        if mode == 5:
+            self.msg = QMessageBox()
+            self.msg.setIcon(QMessageBox.Information)
+            self.msg.setText("Please, select the mode")
+            self.msg.setStandardButtons(QMessageBox.Ok)
+            self.msg.exec_()
+        elif mode == 4:
+            pass
+        else:
+            form.hide()
+            self.minefield = Minefield(mode)
+            self.minefield.close_btn.clicked.connect(lambda: self.destroy_minefield(form))
+            self.minefield.show()
+
+    def destroy_minefield(self, form):
+        #self.minefield.setParent(None)
+        del self.minefield
+        form.show()
 
 
 def main():
--- minefield.py
+++ minefield.py
@@ -1,13 +1,50 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QWidget, QApplication, QSizePolicy, \
+                            QPushButton, QToolButton, QVBoxLayout, QGridLayout
+from PyQt5.QtGui import QIcon
 import sys
+from random import shuffle
 
 class Minefield(QWidget):
-    def __init__(self):
+    def __init__(self, mode):
         super().__init__()
-        self.setWindowTitle('Second window')
-        self.setGeometry(100,100,100,100)
-        btn = QPushButton('try', self)
+        self.mines_map = self.create_mines_map(mode)
+        self.setWindowTitle('Minefield')
+        self.vertical_layout = QVBoxLayout(self)
+        self.grid_layout = QGridLayout()
+        self.grid_layout.setSpacing(0)
+        self.create_field(mode)
+
+
+    def create_field(self, mode):
+        c = 1
+        for x in range(mode[0]):
+            for y in range(mode[1]):
+                button = QToolButton(self)
+                button.setObjectName(str(c))
+                if self.mines_map[c-1] == 1:
+                    button.setIcon(QIcon('mine.png'))
+                size_policy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+                size_policy.setHorizontalStretch(1)
+                size_policy.setHorizontalStretch(1)
+                #size_policy.setHeightForWidth(button.sizePolicy().hasHeightForWidth())
+                button.setSizePolicy(size_policy)
+                self.grid_layout.addWidget(button, x, y)
+                c = c + 1
+
+
+        self.close_btn = QPushButton('close', self)
+        self.vertical_layout.addItem(self.grid_layout)
+        self.vertical_layout.addWidget(self.close_btn)
         self.show()
+
+
+    def create_mines_map(self, mode):
+        clean_cells = [0 for x in range(mode[0] * mode[1] - mode[2])]
+        mines = [1 for x in range(mode[2])]
+        mines_map = (mines + clean_cells)
+        shuffle(mines_map)
+        return mines_map
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
